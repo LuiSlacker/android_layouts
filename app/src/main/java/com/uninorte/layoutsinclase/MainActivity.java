eff625105fafb3888414e4619c25db0870c4fd62
Petulant Slacker <l.goohsen@gmail.com>
2017-08-14 19:26:36 -0500

    adjusted linear layout for functionality

--- app/src/main/java/com/uninorte/layoutsinclase/MainActivity.java
+++ app/src/main/java/com/uninorte/layoutsinclase/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint);
+        setContentView(R.layout.activity_linear);
 
         txt = (TextView) findViewById(R.id.textView);
         queue = new LinkedBlockingQueue<>();
